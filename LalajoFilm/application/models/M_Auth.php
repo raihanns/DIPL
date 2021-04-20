e7861f22fa19a1ed488343f333b4e72320021f6a
raihanns <rainsdotnet@gmail.com>
2021-04-20 13:11:58 +0700

    login

--- LalajoFilm/application/models/M_Auth.php
+++ LalajoFilm/application/models/M_Auth.php
@@ -1,7 +1,7 @@
 <?php
+
 class M_Auth extends CI_model
 {
-
 
     public function login($username)
     {
@@ -12,5 +12,39 @@ class M_Auth extends CI_model
         } else {
             return false;
         }
+    }
+
+    public function getAllUser()
+    {
+        return $this->db->get('user')->result_array();
+    }
+
+    public function insertUser($data)
+    {
+        return $this->db->insert('user', $data);
+    }
+
+    public function editUser($id)
+    {
+        $data = [
+            'nama_depan' => $this->input->post('namadepan', true),
+            'nama_belakang' => $this->input->post('namabelakang', true),
+            'email' => $this->input->post('email', true),
+            'password' => $this->input->post('password')
+        ];
+        $this->db->where('id', $id);
+        $this->db->update('user', $data);
+    }
+
+    public function deleteUser($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('user');
+    }
+
+    public function getUserById($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->get('user')->row_array();
     }
 }
